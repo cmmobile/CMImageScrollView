5378ff31d224b2083f0b8513dbd58340f6a80e6c
Marvin Lin <marvin_lin@cmoney.com.tw>
2020-08-11 11:11:53 +0800

    add: project init

---
+++ CMImageScrollView/CMImageScrollView/CMImageScrollView.h
@@ -0,0 +1,19 @@
+//
+//  CMImageScrollView.h
+//  CMImageScrollView
+//
+//  Created by cm0630 on 2020/8/11.
+//  Copyright © 2020 CM_iOS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMImageScrollView.
+FOUNDATION_EXPORT double CMImageScrollViewVersionNumber;
+
+//! Project version string for CMImageScrollView.
+FOUNDATION_EXPORT const unsigned char CMImageScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMImageScrollView/PublicHeader.h>
+
+
